a03c028b93cda62c00d6379e693c2f1e1e9ff03a
Mateo Fernández-Torreiro <58946811+mfernandeztorreiro@users.noreply.github.com>
2023-02-17 15:24:50 +0000

    Add files via upload

---
+++ plot_circles_w_projplot.py
@@ -0,0 +1,22 @@
+import numpy as np
+import healpy as hp
+from astropy.coordinates import SkyCoord
+
+def plot_circle(rot, r, radec=False):
+  """
+  v1.1 mft 12 07 23
+
+  Plots a non-filled circle on top of a HEALPix map
+
+  rot: center of the circle
+
+  r: radius of the circle (in degrees)
+
+  radec: assumes rot is in J2000 coordinates (default: False)
+  """
+
+  theta = np.linspace(0, 2*np.pi, 1000)
+  lon = rot + r*np.cos(theta)
+  lat = rot + r*np.sin(theta)
+  hp.projplot(lon, lat, lonlat=True)
+  return lon, lat
